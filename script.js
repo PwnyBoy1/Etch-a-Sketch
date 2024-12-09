35da8b8f6293f67eeb11ebbd341c6af15c4dd751
pwnyboy1 <fredi.rivera@hotmail.com>
2024-12-08 22:42:41 -0500

    Add Functionality To Canvas
    Div background color now changes when hovering on
    divs. This function was achived by selecting all
    the cells in the grid and adding an eventlistener
    when they are hovered over; if a div is hovered
    it changes to the color red.

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ for(let i = 0; i < 256; i++){
     div.className = "cell";
     grid.appendChild(div);
 }
+
+const cells = document.getElementsByClassName("cell");
+
+Array.from(cells).forEach(cell => {
+    cell.addEventListener("mouseover", function() {
+        cell.classList.add("red");})
+})
