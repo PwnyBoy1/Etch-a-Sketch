cb58d065b8d371d1a1310e3aa83ed2d7c6dac7b2
pwnyboy1 <fredi.rivera@hotmail.com>
2024-12-11 23:22:01 -0500

    Add Function To Prompt User To Input Desired Canvas Size
    Added a prompt function to request the user to
    input desired canvas size. Individual canvas cells
    are not properly sized to fit canvas. Future
    changes will address said

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const grid = document.querySelector(".grid"); 
+//let gridSize = 16;
+let gridSize = prompt("Please Enter Preferred Grid Size.", "16")
 
-for(let i = 0; i < 256; i++){
+function createGrid (){
+    grid.innerHTML = "";
+    for(let i = 0; i < gridSize * gridSize; i++){
     const div = document.createElement("div");
     div.className = "cell";
     grid.appendChild(div);
 }
+}
 
+createGrid();
 const cells = document.getElementsByClassName("cell");
 
 Array.from(cells).forEach(cell => {
     cell.addEventListener("mouseover", function() {
         cell.classList.add("red");})
 })
+
+
